41653709997cdca34b13ae182c446d17dd086954
FrankIglesias <franciscoj.iglesias@hotmail.com>
2016-04-11 00:04:29 -0300

    Cambios en cgp

--- src/main/java/CGP.java
+++ src/main/java/CGP.java
@@ -13,17 +13,17 @@ public class CGP extends POI {
 	public boolean estaDisponible(Date horario, Servicio servicio) {
 		Servicio servicioBuscado = this.buscarServicio(servicio);
 		return (servicioBuscado.estasDisponibleEn(horario));
-			
+
 	}
 
 	public boolean estaDisponible(Date horario) {
 
 		return servicios.stream().anyMatch(servicio -> servicio.estasDisponibleEn(horario));
 	}
-	
-	private Servicio buscarServicio(Servicio servicio){
-	Servicio elServicioBuscado = servicios.stream().filter(unServicio -> equals(servicio)).findFirst().get();
-	return elServicioBuscado;	
-	
-}
+
+	private Servicio buscarServicio(Servicio servicio) {
+		Servicio elServicioBuscado = servicios.stream().filter(unServicio -> equals(servicio)).findFirst().get();
+		return elServicioBuscado;
+
 	}
+}
