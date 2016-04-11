db4cc232057aae82656fb806fea5f6af7705972d
Roxana <Roxana@Roxana-PC>
2016-04-10 22:56:08 -0300

    cambios para juani

--- src/main/java/BuscarPoi.java
+++ src/main/java/BuscarPoi.java
@@ -1,4 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class BuscarPoi {
  private static  int localizacion;
+private  static List<POI> puntosDeInteres = new ArrayList<POI>();
+ 
+ public static List<POI> buscarSegunPalabraClave(String unaPalabra) {
+		
+		List<POI> puntosSegunPalabra = new ArrayList<POI>();
+		puntosSegunPalabra = puntosDeInteres.stream().filter(unPunto -> unPunto.tenesUnaPalabra(unaPalabra))
+				.collect(Collectors.toList());
+		return puntosSegunPalabra;
+
+	}
+
 }
--- src/main/java/POI.java
+++ src/main/java/POI.java
@@ -1,50 +1,51 @@
 import java.util.ArrayList;
-import java.util.List;
+
 
 public abstract class POI {
-protected Direccion direccion;
-protected String nombre; 
-protected ArrayList<String> palabrasClave = new ArrayList<String>();
+	protected Direccion direccion;
+	protected String nombre;
+	protected ArrayList<String> palabrasClave = new ArrayList<String>();
 
-	public POI(Direccion dir,String nombre){
+	public POI(Direccion dir, String nombre) {
 		this.direccion = dir;
 		this.nombre = nombre;
-		}
-
-	
-	public void distanciaHacia(POI unPoi){
-		//distanciaEntre (direccion , unPoi.direccion);
 	}
-	
-	
-	public double distanciaEntre (Direccion direccion1 , Direccion direccion2){
-		
+
+	public void distanciaHacia(POI unPoi) {
+		// distanciaEntre (direccion , unPoi.direccion);
+	}
+
+	public double distanciaEntre(Direccion direccion1, Direccion direccion2) {
+
 		double lat1 = direccion1.getLatitud();
 		double lat2 = direccion2.getLatitud();
 		double diferenciaLong = Math.toRadians(distancia(direccion1.getLongitud(), direccion2.getLongitud()));
 		double diferenciaLat = Math.toRadians(distancia(lat1, lat2));
-		
+
 		double radio = 6371000;
-		
-		double a = Math.sin(diferenciaLat/2) * Math.sin(diferenciaLat/2) +
-				 Math.cos(lat1) * Math.cos(lat2) * Math.sin(diferenciaLong/2) * 
-				 Math.sin(diferenciaLong/2);
-		
-		double b = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-		
+
+		double a = Math.sin(diferenciaLat / 2) * Math.sin(diferenciaLat / 2)
+				+ Math.cos(lat1) * Math.cos(lat2) * Math.sin(diferenciaLong / 2) * Math.sin(diferenciaLong / 2);
+
+		double b = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
 		double distancia = radio * b;
-		
+
 		return distancia;
-		
+
 	}
-	
-	public double distancia(double num, double num2){
+
+	public double distancia(double num, double num2) {
 		return num - num2;
 	}
-	
-	static double toRadian(double valor){
+
+	static double toRadian(double valor) {
 		return (Math.PI / 180) * valor;
 	}
- 
+
+	
+	public boolean tenesUnaPalabra(String unaPalabra) {
+			return palabrasClave.stream().anyMatch(palabra -> palabra.equals(unaPalabra));
+	}
 
 }
