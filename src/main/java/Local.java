cbf90615faf5c5d5923c9820d684c391eaacef07
FrankIglesias <franciscoj.iglesias@hotmail.com>
2016-04-10 23:18:43 -0300

    Nuevos cambios en Local

--- src/main/java/Local.java
+++ src/main/java/Local.java
@@ -6,15 +6,13 @@ public class Local extends POI {
 	private Rubro rubro;
 	public Local(Direccion dir, String nombre) {
 		super(dir, nombre);
-		// TODO Auto-generated constructor stub
 	}
 	public boolean estaDisponible(Date horario, Servicio servicio) {
-		// TODO Auto-generated method stub
+		
 		return false;
 	}
-	@Override
 	public boolean estaDisponible(Date horario) {
-		// TODO Auto-generated method stub
+
 		return false;
 	}
 }
