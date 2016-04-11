d5cdcebf249bd748a74fe8a8c879902168c819d8
marilu <malupeb@hotmail.com.ar>
2016-04-11 11:45:26 -0300

    Cambie responsabilidad de distanciaEntre de POI a Direccion

--- src/main/java/POI.java
+++ src/main/java/POI.java
@@ -7,50 +7,28 @@ public abstract class POI {
 	protected String nombre;
 	protected ArrayList<String> palabrasClave = new ArrayList<String>();
 
-	public abstract boolean estaDisponible(Date horario, Servicio servicio);
-
-	public abstract boolean estaDisponible(Date horario);
-
 	public POI(Direccion dir, String nombre) {
 		this.direccion = dir;
 		this.nombre = nombre;
 	}
 
-	public static double distancia(double num, double num2) {
-		return num - num2;
+	public abstract boolean estaDisponible(Date horario, Servicio servicio);
+
+	public abstract boolean estaDisponible(Date horario);
+
+	public double distanciaCon(POI unPoi) {
+		return this.direccion.distanciaCon(unPoi.direccion);
 	}
 
 	public boolean tenesUnaPalabra(String unaPalabra) {
 		return palabrasClave.stream().anyMatch(palabra -> palabra.equals(unaPalabra));
 	}
 
-	public static double distanciaEntre(Direccion direccion1, Direccion direccion2) {
-		double earthRadius = 6371000; // radio expresado en metros
-		double lat1 = direccion1.getLatitud();
-		double lat2 = direccion2.getLatitud();
-		double lng1 = direccion1.getLongitud();
-		double lng2 = direccion2.getLongitud();
-
-		// formula de Haversine
-		double dLat = Math.toRadians(lat2 - lat1);
-		double dLng = Math.toRadians(lng2 - lng1);
-		double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(Math.toRadians(lat1))
-				* Math.cos(Math.toRadians(lat2)) * Math.sin(dLng / 2) * Math.sin(dLng / 2);
-		double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-		double dist = (double) (earthRadius * c);
-
-		// retorna distancia en metros
-		return dist;
-
+	public void addPalabraClave(String unaPalabra) {
+		this.palabrasClave.add(unaPalabra);
 	}
-	
-	public void setPalabrasClaves(String unaPalabra)
-	{
-		this.palabrasClave.add(unaPalabra); 
-	}
-	
-	public static void main(String[] args)
-	{
-		
+
+	public static void main(String[] args) {
+
 	}
 }
