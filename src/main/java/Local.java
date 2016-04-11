ca423432d4e6bbf940d71316251502567937f6d0
Roxana <Roxana@Roxana-PC>
2016-04-10 22:48:35 -0300

    .

--- src/main/java/Local.java
+++ src/main/java/Local.java
@@ -1,6 +1,10 @@
 
 public class Local extends POI {
+	
 	private String horario;
 	private Rubro rubro;
-
+	public Local(Direccion dir, String nombre) {
+		super(dir, nombre);
+		// TODO Auto-generated constructor stub
+	}
 }
