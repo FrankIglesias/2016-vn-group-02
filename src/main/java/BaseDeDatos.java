517fe679ac6c1b628f0fa3fdf1937bb71ff8ba2c
Yami <yamilacasarini@gmail.com>
2016-05-13 00:58:31 -0300

    Resultados totales de todas las temrinales

--- src/main/java/BaseDeDatos.java
+++ src/main/java/BaseDeDatos.java
@@ -8,9 +8,11 @@ import java.util.stream.Collectors;
 public class BaseDeDatos {
 	static List<Busqueda> busquedas = new ArrayList<Busqueda>();
 	Map<LocalDate, Integer> reportePorFecha = new HashMap<LocalDate, Integer>();
+	Map<String, Integer> reporteBusquedasTotales = new HashMap<String, Integer>();
+	static List<Terminal> terminales = new ArrayList<Terminal>();
 	Administrador admin;
 
-	BaseDeDatos(){
+	BaseDeDatos() {
 		super();
 		admin = new Administrador();
 	}
@@ -25,14 +27,21 @@ public class BaseDeDatos {
 		return busquedas.stream().filter(unaBusqueda -> unaBusqueda.conFechaDe(fecha)).collect(Collectors.toList())
 				.size();
 	}
-	
-	public void addBusquedasPorFechaAlReporte(LocalDate fecha){
+
+	public void addBusquedasPorFechaAlReporte(LocalDate fecha) {
 		reportePorFecha.put(fecha, this.cantidadDeBusquedasPorFecha(fecha));
 	}
-	
-	public Map<LocalDate, Integer> getReportePorFecha(){
+
+	public Map<LocalDate, Integer> getReportePorFecha() {
 		return reportePorFecha;
 	}
 
-	
+	public void addReportesPorTerminal() {
+
+		for (Terminal unaTerminal : terminales) {
+			reporteBusquedasTotales.put(unaTerminal.getNombre(), unaTerminal.resultadosTotales());
+		}
+
+	}
+
 }
--- src/main/java/Terminal.java
+++ src/main/java/Terminal.java
@@ -3,7 +3,12 @@ import java.util.List;
 
 public class Terminal {
 	
+	String nombre;
 	static List<Integer> reporteParcialPorTerminal = new ArrayList<Integer>();
+	
+	public Terminal(String nombre){
+		this.nombre = nombre;
+	}
 	
 	public int cantidadDeResultadosPorBusqueda(List<POI> puntosSegunPalabra){
 		return puntosSegunPalabra.size();	
@@ -15,5 +20,21 @@ public class Terminal {
 	
 	public List<Integer> getReporteParcialPorTerminal(){
 		return reporteParcialPorTerminal;
+	}
+	
+	public int sumarResultados(List<Integer> enteros){
+		int resultado = 0;
+		for(Integer entero : enteros){
+			resultado += entero;
+		}
+		return resultado;
+	}
+	
+	public int resultadosTotales(){
+		return this.sumarResultados(reporteParcialPorTerminal);
+	}
+	
+	public String getNombre(){
+		return nombre;
 	}
 }
