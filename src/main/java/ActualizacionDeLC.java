42acf07f7fed39a623b043821bdcbd46a3f659f5
yamilacasarini <yamilacasarini@gmail.com>
2016-06-23 21:40:20 -0300

    Cambios en proceso 1

--- src/main/java/ActualizacionDeLC.java
+++ src/main/java/ActualizacionDeLC.java
@@ -14,29 +14,27 @@ public class ActualizacionDeLC {
 	public static void actualizarListaDeLC(RepoLocalesComerciales repositorio) {
 
 		try {
-			
-			
 			File archivo = new File(ruta);
 			FileReader fr = new FileReader(archivo);
 			BufferedReader br = new BufferedReader(fr);
 			String linea;
 
 			while ((linea = br.readLine()) != null) {
-				String[] nombreYpalabras = linea.split(";");
-				if( repositorio.tieneUnLocalConNombre(nombreYpalabras[0]) != null){
-					String[] linea3 = nombreYpalabras[1].split(" ");
-					ArrayList<String> palabrasClave = new ArrayList<String>(Arrays.asList(linea3));
-					repositorio.actualizarLocal(nombreYpalabras[0], palabrasClave);
-				};
+				parsearYAgregarLocal(linea, repositorio);
 			}
 
 		} catch (Exception e) {
 			System.out.println("ERROR");
 		}
 	}
-	
-	
-	public static void proximaLinea() {
+
+	private static void parsearYAgregarLocal(String linea, RepoLocalesComerciales repositorio) {
+		String[] nombreYpalabras = linea.split(";");
+		if (repositorio.tieneUnLocalConNombre(nombreYpalabras[0]) != null) {
+			String[] linea3 = nombreYpalabras[1].split(" ");
+			ArrayList<String> palabrasClave = new ArrayList<String>(Arrays.asList(linea3));
+			repositorio.actualizarLocal(nombreYpalabras[0], palabrasClave);
+		}
 
 	}
 }
