b827ecc77ff71a6a299da00607c442c325efb56e
yamilacasarini <yamilacasarini@gmail.com>
2016-06-29 17:38:12 -0300

    Actualizacion proceso 3

--- src/main/java/AsignarAccionesUsuario/AsignarAccionesUsuarios.java
+++ src/main/java/AsignarAccionesUsuario/AsignarAccionesUsuarios.java
@@ -1,12 +1,13 @@
 package AsignarAccionesUsuario;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TimerTask;
 import java.util.stream.Collectors;
 
 import Repositorio.RepoUsuarios;
 import Repositorio.Usuario;
 
-public class AccionesUsuarios {
+public class AsignarAccionesUsuarios extends TimerTask {
 
 	ArrayList<Acciones> listaDeAcciones = new ArrayList<Acciones>();
 	List<Criterios> listaDeCriterios = new ArrayList<Criterios>();
@@ -16,6 +17,12 @@ public class AccionesUsuarios {
 				.filter(unUsuario -> listaDeCriterios.stream()
 						.allMatch(unCriterio -> unCriterio.esCumplidoPor(unUsuario)))
 				.collect(Collectors.toCollection(ArrayList::new));
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
 	}
 
 	// void cargarAcciones();
