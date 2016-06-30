7fb8a44456b7c4e0c52219d3f883d87000d9501e
Santiago Beitia <santiagob.beitia@gmail.com>
2016-06-30 14:16:34 -0300

    Lo entiendo como terminado (el proceso), no tengo idea de como hacer los tests...

--- src/main/java/Repositorio/DarDeBajaPOIMock.java
+++ src/main/java/Repositorio/DarDeBajaPOIMock.java
@@ -1,16 +1,11 @@
 package Repositorio;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.TimerTask;
+import java.util.Map.Entry;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -19,34 +14,20 @@ import DesignDreamTeamLocation.Geolocalizacion;
 import TypePois.POI;
 import TypePois.genericPOI;
 
-public class DarDeBajaPOI extends TimerTask implements processDarDeBajaPOI {
-	
-	String url;
+public class DarDeBajaPOIMock extends TimerTask implements processDarDeBajaPOI {
+
+	String noProcesado = "[{[\"latitud\":-35.9338322,\"longitud\":72.348353,\"domicilio\":null,\"localidad\":null],1986-04-08 12:30},"
+						+ "{[\"latitud\":-35.9366622,\"longitud\":72.366653,\"domicilio\":null,\"localidad\":null],2016-04-08 12:30}]";
 	Map<Geolocalizacion,LocalDateTime> POIsAEliminar = new HashMap<Geolocalizacion,LocalDateTime>();
 
 	@Override
 	public void run() {
-		String noProcesado = null;
 		System.out.println("Obteniendo datos...");
-		try {
-			noProcesado = httpGet(url);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		if(noProcesado != null)
-		{
-			System.out.println("Procesando datos...");
-			POIsAEliminar = procesarPedido(noProcesado);
-			System.out.println("Por realizarse...");
-			this.eliminarPOIs();
-			System.out.println("Realizado Correctamente");
-		}
-		else
-		{
-			System.out.println("Error!");
-		}
-		
+		System.out.println("Procesando datos...");
+		POIsAEliminar = procesarPedido(noProcesado);
+		System.out.println("Por realizarse...");
+		this.eliminarPOIs();
+		System.out.println("Realizado Correctamente");
 	}
 
 	public Map<Geolocalizacion, LocalDateTime> procesarPedido(String noProcesado) {
@@ -81,26 +62,4 @@ public class DarDeBajaPOI extends TimerTask implements processDarDeBajaPOI {
 			}
 		}
 	}
-
-	public static String httpGet(String urlStr) throws IOException {
-		URL url = new URL(urlStr);
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
-		if (conn.getResponseCode() != 200) {
-			throw new IOException(conn.getResponseMessage());
-		}
-
-		// Buffer the result into a string
-		BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		StringBuilder sb = new StringBuilder();
-		String line;
-		while ((line = rd.readLine()) != null) {
-			sb.append(line);
-		}
-		rd.close();
-
-		conn.disconnect();
-		return sb.toString();
-	}
-
 }
---
+++ src/main/java/Repositorio/processDarDeBajaPOI.java
@@ -0,0 +1,15 @@
+package Repositorio;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import DesignDreamTeamLocation.Geolocalizacion;
+import TypePois.POI;
+
+public interface processDarDeBajaPOI {
+	public Map<Geolocalizacion, LocalDateTime> procesarPedido(String noProcesado);
+	
+	public POI getPOI(Geolocalizacion geo);
+
+	public void eliminarPOIs();
+}
