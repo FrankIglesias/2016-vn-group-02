ca5bfa56a70d3b95818d2a5ec919c2b6ece30064
Santiago Beitia <santiagob.beitia@gmail.com>
2016-06-30 13:35:55 -0300

    .

---
+++ src/main/java/TypePois/genericPOI.java
@@ -0,0 +1,12 @@
+package TypePois;
+
+import DesignDreamTeamLocation.Geolocalizacion;
+
+public class genericPOI extends POI {
+
+	public genericPOI(Geolocalizacion point) {
+		super(point, null, null, null, null);
+		// TODO Auto-generated constructor stub
+	}
+
+}
