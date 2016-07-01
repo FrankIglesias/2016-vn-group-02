11ccd41dd211867a0f06c11e49050db2a14a913a
FrankIglesias <frank.beatle@gmail.com>
2016-07-01 17:30:11 -0300

    cambios para que ande el sistema de dar de baja

--- src/test/java/BajaDePOIsTest.java
+++ src/test/java/BajaDePOIsTest.java
@@ -16,10 +16,11 @@ public class BajaDePOIsTest {
 	Geolocalizacion geo1;
 	Geolocalizacion geo2;
 	DarDeBajaPOIMock mock;
-	
+	int cantidadDePoiAntesDeModificacion;
 	@Before
 	public void init() {
 		repo = RepoPOIs.getInstance();
+		repo.inicializarPuntosDeIntereses();
 		geo1 = new Geolocalizacion(48.2088623046875, 16.369810104370117, null, null);
 		geo2 = new Geolocalizacion(16.399024963378906, 48.21460723876953, null, null);
 		poi1 = new genericPOI(geo1,"poi1");
@@ -29,20 +30,26 @@ public class BajaDePOIsTest {
 	
 	@Test
 	public void RepoCargarDosPOITest(){
+	cantidadDePoiAntesDeModificacion = repo.size();
 		repo.agregarNuevosPoi(poi1);
 		repo.agregarNuevosPoi(poi2);
-		Assert.assertEquals(repo.size(), 2);
+		Assert.assertEquals(repo.size(), cantidadDePoiAntesDeModificacion+2);
 	}
 	
 	@Test
 	public void RepoBorrarDosPOITest(){
+		
+		System.out.println("\n\n\n\n"+cantidadDePoiAntesDeModificacion+"\n\n\n\n");
+		repo.agregarNuevosPoi(poi1);
+		repo.agregarNuevosPoi(poi2);
+		cantidadDePoiAntesDeModificacion= repo.size();
 		mock.run();
-		Assert.assertEquals(repo.size(), 0);
+		Assert.assertEquals(repo.size(), cantidadDePoiAntesDeModificacion -2);
 	}
 	
 	@Test
 	public void RestJsonCorrectamente() throws Exception {
 		String json = mock.obtenerStream();
-		Assert.assertNotEquals("", json);
+		Assert.assertNotEquals("latitud", json);
 	}
 }
