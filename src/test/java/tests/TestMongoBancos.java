c386a518605f3fc0cc14b7f08d4605b8f4e481c0
guest-N9PwwN <guest-N9PwwN@alumnodisenio-VirtualBox>
2016-11-26 11:37:14 -0300

    Agrego la linea para yami

--- src/test/java/tests/TestMongoBancos.java
+++ src/test/java/tests/TestMongoBancos.java
@@ -10,7 +10,9 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
+import com.mongodb.util.JSON;
 
 import BancoExterno.ApiDeBancoReal;
 import BancoExterno.BancoAdapter;
@@ -35,6 +37,7 @@ public class TestMongoBancos {
 
 		unBanco = jsonFactory.fromJson(bancos[0].substring(1).concat("}"), BancoAdapter.class);
 		ParserBanco.guardarBanco(unBanco);
+		DBObject dbObject = (DBObject) JSON.parse("{'name':'mkyong', 'age':30}");
 		MongoClient cliente = new MongoClient();
 		DB database = cliente.getDB("Banco");
 		DBCollection collection = database.getCollection("Banco");
