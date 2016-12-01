1d2acb8e06319e38aecb367c9477145dd421d66b
mapeb <malupeb@hotmail.com.ar>
2016-12-01 00:52:16 -0300

    avance importante

--- src/main/java/MainPackage/Main.java
+++ src/main/java/MainPackage/Main.java
@@ -1,3 +1,6 @@
+package MainPackage;
+
+import static spark.Spark.*;
 import static spark.Spark.get;
 import static spark.Spark.port;
 import static spark.Spark.staticFileLocation;
@@ -6,13 +9,26 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class Main {
 
+	
 	public static void main(String[] args) {
+		System.out.println("Iniciando servidor");
+		
+		HandlebarsTemplateEngine engine = new HandlebarsTemplateEngine();
+	    MainController home = new MainController();
+	    
+	    staticFileLocation("/templates");
+		
+        get("/hello", home::mostrar, engine);
+    }
+}
+	
+	/*public static void main(String[] args) {
 	    System.out.println("Iniciando servidor");
 	    HandlebarsTemplateEngine engine = new HandlebarsTemplateEngine();
 	    MainController home = new MainController();
-	    port(8080);
+	    port(5050);
 
-	    staticFileLocation("/public");
+	   // staticFileLocation("/public");
 
 	    get("/", home::mostrar, engine);
 	    get("/index.html", (request, response) -> {
@@ -20,6 +36,6 @@ public class Main {
 	      return null;
 	    });
 
-	}
+	}*/
 
-}
+//}
--- src/main/java/MainPackage/MainController.java
+++ src/main/java/MainPackage/MainController.java
@@ -1,10 +1,12 @@
+package MainPackage;
+
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 
 public class MainController {
 	  public ModelAndView mostrar(Request request, Response response) {
-		    return new ModelAndView(null, "Frank.hbs");
+		    return new ModelAndView(null, "home.hbs");
 		  }
 
 }
