3adab86dac649c5b14a018ee76e4e87e6a2093ef
FrankIglesias <frank.beatle@gmail.com>
2016-12-20 23:10:34 -0300

    agrego ultima cosa del main

--- src/main/java/MainPackage/Main.java
+++ src/main/java/MainPackage/Main.java
@@ -24,12 +24,12 @@ public class Main {
 		  Spark.get("/admin_terminales", home::mostrarTerminales, engine);
 		  Spark.get("/editar_terminal", home::mostrarEditarTerminal, engine);
 		  Spark.get("/admin_acciones", home::mostrarAdminAcciones, engine);
-		 // Spark.get("/admin_pois", home::mostrarPois, engine);
+		//  Spark.get("/admin_pois", home::mostrarPois, engine);
 		  Spark.get("/admin_consultas", home::mostrarConsultas, engine);
 		  Spark.get("/buscar_pois", home::filtrarNombreTipoPois, engine);
 		  Runtime rt = Runtime.getRuntime();
 		  try {
-			Process pr = rt.exec("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe http:\\\\localhost:"+puerto.toString());
+			Process pr = rt.exec("\"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe\" http:\\\\localhost:"+puerto.toString());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
