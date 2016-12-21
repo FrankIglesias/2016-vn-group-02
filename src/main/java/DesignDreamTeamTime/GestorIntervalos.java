f230358163be7e2b0e89d5e83640d19ca19f82b2
yamilacasarini <yamilacasarini@gmail.com>
2016-12-20 22:57:13 -0300

    sos una mierda hashmap

--- src/main/java/DesignDreamTeamTime/GestorIntervalos.java
+++ src/main/java/DesignDreamTeamTime/GestorIntervalos.java
@@ -2,14 +2,17 @@ package DesignDreamTeamTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.ElementCollection;
+import javax.persistence.Embeddable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Transient;
 
 
 @Entity
-
 public class GestorIntervalos {
 	
 	@Id
@@ -17,8 +20,8 @@ public class GestorIntervalos {
 	public int id;
 	
 	
-	@ElementCollection
 	//@CollectionTable(name="intervalosHorarios", joinColumns=@JoinColumn(name="id_poi"))
+	@OneToMany(cascade = CascadeType.ALL)
 	public List<IntervaloHorario> intervalosHorarios = new ArrayList<IntervaloHorario> ();
 
 	public List<IntervaloHorario> getIntervalosHorarios() {
--- src/main/java/TypePois/Servicio.java
+++ src/main/java/TypePois/Servicio.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
