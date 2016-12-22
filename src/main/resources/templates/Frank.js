b2230b8de63c9acf56efdafecd8fc64f8405e005
Juani Cabanas <juani_cabanas@hotmail.com>
2016-12-22 00:34:03 -0300

    vamo que verifica la contra bb

--- src/main/resources/templates/Frank.js
+++ src/main/resources/templates/Frank.js
@@ -1,10 +1,17 @@
 function fucionValidarUsuario(){
+	if(document.getElementById('contraUsuario').value=="")
+		alert("Ingrese una contrasena");
+	else
+		{
 	if(document.getElementById('nombreDeUsuario').value == 'admin'){
 			window.location.href = "administrador.hbs";
 	}else {
 		window.location.href = "usuario?nombreFiltro="+document.getElementById('nombreDeUsuario').value;
 	}
+		}
+
 }
+
 
 	
 
