539bfdc6025299831016df74f75a19998d9de072
FrankIglesias <frank.beatle@gmail.com>
2016-12-22 00:56:39 -0300

    agrego funcion go back

--- src/main/resources/templates/Frank.js
+++ src/main/resources/templates/Frank.js
@@ -1,18 +1,17 @@
-function fucionValidarUsuario(){
-	if(document.getElementById('contraUsuario').value=="")
+function fucionValidarUsuario() {
+	if (document.getElementById('contraUsuario').value == "")
 		alert("Ingrese una contrasena");
-	else
-		{
-	if(document.getElementById('nombreDeUsuario').value == 'admin'){
+	else {
+		if (document.getElementById('nombreDeUsuario').value == 'admin') {
 			window.location.href = "administrador.hbs";
-	}else {
-		window.location.href = "usuario?nombreFiltro="+document.getElementById('nombreDeUsuario').value;
-	}
+		} else {
+			window.location.href = "usuario?nombreFiltro="
+					+ document.getElementById('nombreDeUsuario').value;
 		}
+	}
 
 }
 
-
-	
-
-	
+function goBack() {
+	window.history.back();
+}
