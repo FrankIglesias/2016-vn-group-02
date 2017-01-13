27364095b5a668285b0c2373fb55ba03fd5ab5d5
FrankIglesias <frank.beatle@gmail.com>
2017-01-13 04:06:19 -0300

    agrego mas detalles de administrador

--- src/main/java/DesignDreamTeamTime/Feriado.java
+++ src/main/java/DesignDreamTeamTime/Feriado.java
@@ -43,6 +43,36 @@ public class Feriado {
 	{
 		return this.mes;
 	}
+	public String getMesString(){
+		switch(mes){
+		case 1:
+			return "Enero";
+		case 2:
+			return "Febrero";
+		case 3:
+			return "Marzo";
+		case 4:
+			return "Abril";
+		case 5:
+			return "Mayo";
+		case 6:
+			return "Junio";
+		case 7:
+			return "Julio";
+		case 8:
+			return "Agosto";
+		case 9:
+			return "Septiembre";
+		case 10:
+			return "Octubre";
+		case 11:
+			return "Noviembre";
+		case 12:
+			return "Diciembre";
+		default:
+			return "";
+		}
+	}
 	public boolean comparateConDiaYMes(LocalDate unaFecha) {
 		return (dia == unaFecha.getDayOfMonth() && mes == unaFecha.getMonthValue());
 	}
